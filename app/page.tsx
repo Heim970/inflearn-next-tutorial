379d1295ca48e5537d9fe1c86ba641255e0fc5fc
Hyemin Jeong <porlife13@gmail.com>
2025-03-09 08:40:22 +0900

    2025-03-08 section2-3

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Next.js Tutorial",
+  description: "Next.js 튜토리얼 배우기",
+};
 
 export default function Home() {
   return (
